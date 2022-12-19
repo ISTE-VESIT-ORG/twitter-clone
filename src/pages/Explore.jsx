52d8f3b779fa31ec1f025d33f95ff2562adf43df
Sarvesh Patil <sarvesh2902@gmail.com>
2022-12-19 12:56:12 +0530

    initial commit

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Explore = () => {
+    return <Layout title="Explore / Twitter">Explore</Layout>;
+};
+
+export default Explore;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Home = () => {
+    return <Layout title="Home / Twitter">Home</Layout>;
+};
+
+export default Home;
