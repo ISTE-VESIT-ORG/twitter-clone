92b7e253a700b533d81fcd648bb6326cee01dd63
Hrishikesh kumbhar <hrishikumbhar156@gmail.com>
2022-12-19 14:18:17 +0530

    Done menuitems and logo

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const MenuItem = ({icon,title,link}) => {  
+  return (
+    <Link to={link}>
+      <div className="flex flex-row hover:bg-slate-200 rounded-full cursor-pointer p-3 ">
+        {/* icon  */}
+        {icon}
+        {/* title  */}
+        <h1 className="text-xl font-medium px-3">{title}</h1>
+      </div>
+     
+    </Link>
+  );
+};
+
+export default MenuItem;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
-
+import Sidebar from "../components/Sidebar";
 const Home = () => {
-    return <Layout title="Home / Twitter">Home</Layout>;
+    return <Layout title="Home / Twitter">
+        <div className="h-screen flex flex-row">
+            {/* Sidebar  */}
+            <Sidebar />
+            {/* Homefeed Section  */}
+
+            {/* Right Explore bar */}
+
+        </div>
+    </Layout>;
 };
 
 export default Home;
